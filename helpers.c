107e8a3ffd9e0706b1192e744852e89951200248
ahaad14550-boop <ahaad.14450@gmail.com>
2026-04-16 11:40:32 +0000

    feat: add helpers.c with print_int function for task 1

--- helpers.c
+++ helpers.c
@@ -1,31 +1,54 @@
 #include "main.h"
 
 /**
- * send_char - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1
+ * _putchar - writes character to stdout
+ * @c: char to print
+ * Return: 1
  */
-int send_char(char c)
+int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
- * put_text - prints a string
- * @str: The string to print
- * Return: Number of characters printed
+ * print_char - prints a char
+ * @args: va_list
+ * Return: 1
  */
-int put_text(char *str)
+int print_char(va_list args)
 {
-	int i = 0;
-
-	if (str == NULL)
-		str = "(null)";
-
-	while (str[i])
-	{
-		send_char(str[i]);
-		i++;
-	}
-	return (i);
+return (_putchar(va_arg(args, int)));
 }
+
+/**
+ * print_str - prints a string
+ * @args: va_list
+ * Return: number of chars printed
+ */
+int print_str(va_list args)
+{
+char *s = va_arg(args, char *);
+int i = 0;
+
+if (!s)
+s = "(null)";
+while (s[i])
+{
+_putchar(s[i]);
+i++;
+}
+return (i);
+}
+
+/**
+ * print_pct - prints percent sign
+ * @args: va_list (unused)
+ * Return: 1
+ */
+int print_pct(va_list args)
+{
+(void)args;
+return (_putchar('%'));
+}
+
+
